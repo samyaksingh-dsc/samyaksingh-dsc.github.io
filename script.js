432bba91e825132cfff2609908e04718c74a2c2d
Samyak Singh <samyaksingh.dsc@gmail.com>
2024-08-04 17:50:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// Smooth scrolling for anchor links
+$('a[href*="#"]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 500, 'linear');
+});
 
